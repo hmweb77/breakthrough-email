28ebf8d3fc261e4fa4740d3dc2eec46bbc73e02f
hmweb77 <oussamahamdani1@gmail.com>
2025-05-03 17:30:03 +0100

    adding google analytics

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,30 @@
 'use client';
+import Head from 'next/head';
 import HeroSection from '../components/Hero';
 import WaitlistForm from '../components/EmailList.jsx';
 
 
 export default function HomePage() {
   return (
+    <>
+    <Head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-1CYPPH7D2K"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-1CYPPH7D2K');
+            `,
+          }}
+        />
+      </Head>
     <main>
       <HeroSection />
       <WaitlistForm/>
     </main>
+    </>
   );
 }
